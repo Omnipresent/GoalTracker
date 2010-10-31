dd44e1dfce0520febbbc3126d7d9d67faa0df958
Bhaarat Sharma <bhaarat.s@gmail.com>
2010-10-30 20:35:32 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Goaltracker::Application.config.secret_token = '6ec267d88b0bd73ebdcffe7c4e6073a9d0e99ba1f7ddafb55aef440089a44d8cb8f56874b6da21313193557fc7be340afb1f74c34d20cce7b9c60e2328e1be96'
